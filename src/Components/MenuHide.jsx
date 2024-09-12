54d7c22b1825a5bad4fb87422edd1cb42a532312
BokeeJ <bokeejusthard@gmail.com>
2024-09-12 23:21:57 +0200

    homepage

--- src/Components/MenuHide.jsx
+++ src/Components/MenuHide.jsx
@@ -5,41 +5,42 @@ function MenuHide() {
     const [isVisible, setIsVisible] = useState(false);
 
     const handleScroll = () => {
-
-        setIsVisible(window.scrollY > 170);
+        setIsVisible(window.scrollY > 20);
     };
 
     useEffect(() => {
         window.addEventListener('scroll', handleScroll);
-
         return () => window.removeEventListener('scroll', handleScroll);
     }, []);
 
     return (
         <div
-            className={` fixed top-0 left-0 w-full flex justify-center right-0 top-0 bg-gray-200 text-white p-4 transition-transform duration-1000 ease-in-out z-50 ${isVisible ? 'translate-y-0' : '-translate-y-full'
+            className={`fixed top-0 left-0 w-full flex justify-center text-white p-4 transition-transform duration-1000 ease-in-out z-50 ${isVisible ? 'translate-x-0' : '-translate-x-full'
                 }`}
         >
-            <div>
-                <ul className=' flex lg:gap-5  gap-2 p-3  font-roboto '>
-                    <NavLink to={'/'} className="relative group">
-                        <a href="/pocetna" className="text-black">POČETNA</a>
+            {/* Društvene mreže */}
+
+            {/* Mini meni */}
+            <div className='flex justify-center items-center '>
+                <ul className='flex lg:gap-5 gap-2 p-3 font-roboto'>
+                    <NavLink to="/" className="relative group">
+                        <span className="text-black">POČETNA</span>
                         <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-black transition-all duration-300 group-hover:w-full"></span>
                     </NavLink>
-                    <NavLink to={'/tim'} className="relative group">
-                        <a href="/tim" className="text-black">NAŠ TIM</a>
+                    <NavLink to="/tim" className="relative group">
+                        <span className="text-black">NAŠ TIM</span>
                         <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-black transition-all duration-300 group-hover:w-full"></span>
                     </NavLink>
-                    <NavLink to={'/usluge'} className="relative group">
-                        <a href="/usluge" className="text-black">USLUGE</a>
+                    <NavLink to="/usluge" className="relative group">
+                        <span className="text-black">USLUGE</span>
                         <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-black transition-all duration-300 group-hover:w-full"></span>
                     </NavLink>
-                    <NavLink to={'/galerija'} className="relative group">
-                        <a href="/galerija" className="text-black">GALERIJA</a>
+                    <NavLink to="/galerija" className="relative group">
+                        <span className="text-black">GALERIJA</span>
                         <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-black transition-all duration-300 group-hover:w-full"></span>
                     </NavLink>
-                    <NavLink to={'/kontakt'} className="relative group">
-                        <a href="/kontakt" className="text-black">KONTAKT</a>
+                    <NavLink to="/kontakt" className="relative group">
+                        <span className="text-black">KONTAKT</span>
                         <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-black transition-all duration-300 group-hover:w-full"></span>
                     </NavLink>
                 </ul>
--- src/Pages/Pocetna.jsx
+++ src/Pages/Pocetna.jsx
@@ -2,9 +2,36 @@ import React from 'react'
 
 function Home() {
     return (
-        <div className='bg-cover relative bg-center h-screen bg-[url(/pocetna.webp)]'>
+        <div className='bg-cover relative bg-center h-screen  bg-[url(/pocetna.webp)] flex justify-center items-center opacity-'>
             <div className='absolute inset-0 bg-gradient-to-b from-white via-transparent to-transparent rounded-md'></div>
-            <h1 className='text-black text-2xl relative'>caocao</h1>
+            <div className='absolute p-2 grid grid-cols-2 lg:grid-cols-4 gap-2 justify-center items-center mt-10 opacity-80'>
+                <div className='bg-white lg:p-5 p-1 flex justify-center flex-col items-center shadow-xl shadow-black rounded-l-xl  hover:m-1 transition-all duration-700 lg:w-[250px] lg:h-[250px] w-[130px] h-[130px]'>
+                    <img src="/zub1.svg" alt="zub" className='lg:w-[150px] lg:h-[150px] w-[80px] h-[80px]' />
+                    <h3 className='lg:text-xl  font-bold font-roboto'>IZBELJIVANJE</h3>
+                    <hr className='w-[80%] border-black border m-2' />
+                    <h5 className='font-bold font-roboto lg:block hidden'>ORDINACIJSKO I KUĆNO</h5>
+                </div>
+                <div className='bg-white  lg:p-5 p-1 flex justify-center flex-col items-center shadow-xl shadow-black hover:m-1 transition-all duration-700 lg:w-[250px] lg:h-[250px] w-[130px] h-[130px] lg:rounded-none rounded-r-xl'>
+                    <img src="/zub2.svg" alt="zub" className='lg:w-[150px] lg:h-[150px] w-[80px] h-[80px]' />
+                    <h3 className='lg:text-xl font-bold font-roboto'>PREVENTIVA</h3>
+                    <hr className='w-[80%] border-black border m-2' />
+                    <h5 className='font-bold font-roboto  lg:block hidden'>BESPLATAN PREGLED I SAVET</h5>
+                </div>
+                <div className='bg-white lg:p-5 p-1 flex justify-center flex-col items-center shadow-xl shadow-black lg:rounded-none rounded-l-xl hover:m-1 transition-all duration-700 lg:w-[250px] lg:h-[250px] w-[130px] h-[130px]'>
+                    <img src="/zub3.svg" alt="zub" className='lg:w-[150px] lg:h-[150px] w-[80px] h-[80px]' />
+                    <h3 className='lg:text-xl font-bold font-roboto'>LEČENJE ZUBA</h3>
+                    <hr className='w-[80%] border-black border m-2' />
+                    <h5 className='font-bold font-roboto  lg:block hidden'>LEČENJE, POPRAVKA, VAĐENJE</h5>
+                </div>
+                <div className='bg-white lg:p-5 p-1 flex justify-center flex-col items-center shadow-xl shadow-black rounded-r-xl hover:m-1 transition-all duration-700 lg:w-[250px] lg:h-[250px] w-[130px] h-[130px]'>
+                    <img src="/zub4.svg" alt="zub" className='lg:w-[150px] lg:h-[150px] w-[80px] h-[80px]' />
+                    <h3 className='lg:text-xl font-bold font-roboto'>PROTETIKA</h3>
+                    <hr className='w-[80%] border-black border m-2' />
+                    <h5 className='font-bold font-roboto  lg:block hidden'>ZUBNA PROTETIKA</h5>
+                </div>
+
+
+            </div>
         </div>
     )
 }
